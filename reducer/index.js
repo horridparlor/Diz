0ad5118aea94103065e17948e3ae42d2c5e9ce16
Eero Laine <eero.laine@tuni.fi>
2023-05-11 18:39:47 +0300

    Added options to reduceer, Closes #3

--- reducer/index.js
+++ reducer/index.js
@@ -1,5 +1,30 @@
 function reduce() {
     var words = document.getElementById("words");
-    words.value = words.value.replace(/ |\n/g, "");
-    console.log(222, words);
+    const filterOuts = getFilters();
+    filterOuts.forEach(filter => {
+        words.value = words.value.replaceAll(filter, "");
+    })
+}
+
+function getFilters() {
+    var filterOuts = [];
+    if (document.getElementById("rmSpace").checked) {
+        filterOuts.push(" ");
+    }
+    if (document.getElementById("rmBreak").checked) {
+        filterOuts.push("\n");
+    }
+    const customValue = document.getElementById("rmCustom").value;
+    if (customValue) {
+        filterOuts.push(customValue);
+    }
+    return filterOuts;
+}
+
+function onTextInput(id) {
+    reduce();
+    focusInput(id);
+    if (id !== "words") {
+        focusInput("words");
+    }
 }
